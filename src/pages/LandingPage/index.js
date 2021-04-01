dac2cdb03d989310e84596c62c824f2b0cae2908
Geraldo Luiz <geraldobl58@gmail.com>
2021-04-01 09:36:45 -0300

    feat: finalizing layout

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -10,11 +10,13 @@ import logoImg from '../../assets/images/logo.png';
 
 export function LandingPage() {
   return (
-    <>
-      <TopNav />
-      <img className={styles.logo} src={logoImg} alt="Logo" />
-      <Search />
-      <SearchSuggestions />
-    </>
+    <div className={styles.landing}>
+      <div className={styles['search-area']}>
+        <TopNav />
+        <img className={styles.logo} src={logoImg} alt="Logo" />
+        <Search />
+        <SearchSuggestions />
+      </div>
+    </div>
   );
 }
