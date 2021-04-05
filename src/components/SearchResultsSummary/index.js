9b11e8871f8bfebeaa3ba4b9890b6999fe603129
Geraldo Luiz <geraldobl58@gmail.com>
2021-04-05 14:01:24 -0300

    feat: finish search results summary component

---
+++ src/components/SearchResultsSummary/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import styles from './SearchResultsSummary.module.css';
+
+export function SearchResultsSummary() {
+  return (
+    <div className={styles.container}>
+      <div className={styles['search-summary']}>
+        <h1 className="subtitle">
+          <strong>hamburgers</strong> vila rica
+        </h1>
+        <p>Mostrando 1-20 de 534 resultados</p>
+      </div>
+      <div className={styles.filters}>
+        <button type="button" className="button">
+          <span className="icon">
+            <i className="fas fa-sliders-h" />
+          </span>
+          <span>Todos</span>
+        </button>
+        <div className="buttons has-addons">
+          <button type="button" className="button">
+            R$
+          </button>
+          <button type="button" className="button">
+            R$ R$
+          </button>
+          <button type="button" className="button">
+            R$ R$ R$
+          </button>
+        </div>
+        <button type="button" className="button">
+          <span className="icon">
+            <i className="fas fa-clock" />
+          </span>
+          <span>Aberto</span>
+        </button>
+        <button type="button" className="button">
+          <span className="icon">
+            <i className="fas fa-dollar-sign" />
+          </span>
+          <span>Preços</span>
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import { NavBar } from '../../components/NavBar';
 import { SubNav } from '../../components/SubNav';
+import { SearchResultsSummary } from '../../components/SearchResultsSummary';
 
 export function Search() {
   return (
     <div>
       <NavBar />
       <SubNav />
+      <SearchResultsSummary />
     </div>
   );
 }
